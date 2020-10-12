6c4d11ca9a2e22f9015f516f625acfeb8a81cec6
Yurei-Liu <71692471+Yurei-Liu@users.noreply.github.com>
2020-10-13 04:47:03 +0800

    2020/10/13
    register.js, register.jsp

--- mountainEX_Project/src/main/webapp/member/register/register.js
+++ mountainEX_Project/src/main/webapp/member/register/register.js
@@ -1,19 +1,20 @@
-function checkName(){
-    let theNameObj = document.getElementById("memberId");
-    let theNameObjVal = theNameObj.value;
-    let theNameObjValLen = theNameObjVal.length;
+// 帳號型態確認
+function checkMbID(){
+    let theMbIdObj = document.getElementById("memberId");
+    let theMbIdObjVal = theMbIdObj.value;
+    let theMbIdObjValLen = theMbIdObjVal.length;
     let sp = document.getElementById("mbIdsp");
     let flag = false;
 
-    if(theNameObjVal == ""){
+    if(theMbIdObjVal == ""){
         sp.innerHTML = "請輸入帳號";
-    } else if(theNameObjValLen >= 6){
-        for(let i = 0; i < theNameObjValLen; i++){
+    } else if(theMbIdObjValLen >= 6){
+        for(let i = 0; i < theMbIdObjValLen; i++){
             let re = new RegExp(/[A-Za-z]+[0-9]/);
             let re2 = new RegExp(/[0-9]+[A-Za-z]/);
-            if(theNameObjVal.match(re)){
+            if(theMbIdObjVal.match(re)){
                 flag = true;
-            }else if(theNameObjVal.match(re2)){
+            }else if(theMbIdObjVal.match(re2)){
                 flag = true;
             }else{
                 flag = false;
@@ -22,11 +23,100 @@ function checkName(){
             if(flag){
                 sp.innerHTML = "正確";
             }else{
-                sp.innerHTML = "帳號必須包含大小寫英文與數字";
+                sp.innerHTML = "錯誤，帳號必須包含大小寫英文與數字";
             }
         }
     }else{
-        sp.innerHTML = "密碼長度最少6個字元";
+        sp.innerHTML = "帳號長度最少6個字元";
     }
 
+}
+
+
+// 密碼型態確認
+function checkPwd(){
+    let thePwdObj = document.getElementById("password");
+    let thePwdObjVal = thePwdObj.value;
+    let thePwdObjValLen = thePwdObjVal.length;
+    let sp = document.getElementById("pdsp");
+    let flag = false;
+
+    if(thePwdObjVal == ""){
+        sp.innerHTML = "請輸入密碼";
+    }else if(thePwdObjValLen >= 8){
+        let re = new RegExp(/[A-Za-z0-9_]+[!@#$%^&*]/);
+        let re2 = new RegExp(/[!@#$%^&*]+[A-Za-z0-9_]/);
+        if(thePwdObjVal.match(re)){
+            flag = true;
+        }else if(thePwdObjVal.match(re2)){
+            flag = true;
+        }else{
+            flag = false;
+        }
+
+        if(flag){
+            sp.innerHTML = "正確";
+        }else{
+            sp.innerHTML = "錯誤，密碼請包含大小寫英文字母、數字與特殊字元_!@#$%^&*";
+        }
+    }else{
+        sp.innerHTML = "密碼長度最少8個字元";
+    }
+}
+
+// 密碼二次確認
+function doublePwd(){
+    let theDPwdObj = document.getElementById("pwdCheck");
+    let theDPwdObjVal = theDPwdObj.value;
+    let thePwd1Obj = document.getElementById("password");
+    let thePwd1ObjVal = thePwd1Obj.value;
+    let sp = document.getElementById("pwd2sp");
+    let flag = false;
+
+    if(theDPwdObjVal == ""){
+        sp.innerHTML = "請再次輸入密碼";
+    }else if(theDPwdObjVal == thePwd1ObjVal){
+        flag = true;
+    }else{
+        flag = false;
+    }
+
+    if(flag){
+        sp.innerHTML = "密碼相符";
+    }else{
+        sp.innerHTML = "密碼不相符";
+    }
+
+}
+
+
+//確認姓名型態
+function checkName(){
+    let theNameObj = document.getElementById("name");
+    let theNameObjVal = theNameObj.value;
+    let theNameObjValLen = theNameObjVal.length;
+    let sp = document.getElementById("nmsp");
+    let flag = false;
+
+    if(theNameObjVal == ""){
+        sp.innerHTML = "請輸入姓名";
+    }else if(theNameObjValLen >= 2){
+        for(let i = 0; i < theNameObjValLen; i++){
+            let ch = theNameObjVal.charCodeAt(i);
+            if(ch >= 0x4e00 && ch <= 0x9fff){
+                flag = true;
+            }else{
+                flag = false;
+                break;
+            }
+        }
+
+        if(flag){
+            sp.innerHTML = "正確";
+        }else{
+            sp.innerHTML = "請輸入中文字元";
+        }
+    }else{
+        sp.innerHTML = "姓名最少輸入兩個字元";
+    }
 }
--- mountainEX_Project/src/main/webapp/member/register/register.jsp
+++ mountainEX_Project/src/main/webapp/member/register/register.jsp
@@ -24,47 +24,47 @@
     <div>
         <form method="POST" action="./RegisterServletMP">
 			<label>帳號：</label>
-			<input type="text" name="memberId" id="memberId" size="20" value="${param.memberId}" class="fieldWidth" placeholder="請輸入姓名" autocomplete="off" onblur="checkName()">
+			<input type="text" required="required" name="memberId" id="memberId" size="20" class="fieldWidth" placeholder="請輸入帳號" autocomplete="off" onblur="checkMbID()">
 			<span id="mbIdsp"></span>
 			<br/>
 		
 			<label>密碼：</label>
-			<input type="password" name="password" id="password" size="20" value="${param.password}" class="fieldWidth" placeholder="請輸入密碼" onblur="checkPwd()">
-			<span id="pwdsp"></span>
+			<input type="password" required="required" name="password" id="password" size="20" class="fieldWidth" placeholder="請輸入密碼" onblur="checkPwd()">
+			<span id="pdsp"></span>
 			<br/>
 		
 			<label>密碼確認：</label>
-			<input type="pwdCheck" name="pwdCheck" id="pwdCheck" size="20" class="fieldWidth" placeholder="請再次輸入密碼" onblur="doublePwd()">
+			<input type="password" required="required" name="pwdCheck" id="pwdCheck" size="20" class="fieldWidth" placeholder="請再次輸入密碼" onblur="doublePwd()">
 			<span id="pwd2sp"></span>
 			<br/>
 		
 			<label>姓名：</label>
-			<input type="text" name="name" id="name" value="${param.name}" size="20" class="fieldWidth" onblur="checkName()">
+			<input type="text" required="required" name="name" id="name" value="${param.name}" size="20" class="fieldWidth" placeholder="請輸入姓名" onblur="checkName()">
 			<span id="nmsp"></span>
 			<br/>
 			
 			<label>地址：</label>
-			<input type="text" name="address" id="address" size="30">
+			<input type="text" required="required" name="address" id="address" placeholder="請輸入收件地址" size="30">
 			<br/>
 		
 			<label>電話：</label>
-			<input type="text" name="tel" id="tel" size="20" class="fieldWidth">
+			<input type="text" required="required" name="tel" id="tel" size="20" placeholder="請輸入聯絡電話" class="fieldWidth">
 			<span id="tlsp"></span>
 			<br/>
 		
 			<label>Email：</label>
-			<input type="email" name="email" id="email" value="${param.email}" size="20" class="fieldWidth" >
+			<input type="email" required="required" name="email" id="email" placeholder="請輸入電子信箱" size="20" class="fieldWidth" >
 			<span id="emailsp"></span>
 			<br/>
 		
 			<label>登山經驗：</label>
-			<input type="text" name="exp" id="exp" value="${param.exp}" size="20" class="fieldWidth">
+			<input type="text" required="required" name="exp" id="exp" placeholder="請輸入登山經驗" size="20" class="fieldWidth">
 			<span id="expsp"></span>
 			<br/>
 		
 			<label>身分組選擇</label>
-			<input type="radio" id="groupId" name="groupId" value="100">一般會員
-			<input type="radio" id="groupId" name="groupId" value="200">登山嚮導
+			<input type="radio" required="required" id="groupId" name="groupId" value="100">一般會員
+			<input type="radio" required="required" id="groupId" name="groupId" value="200">登山嚮導
 <!-- 			<select name="groupId"> -->
 <!-- 				<option value="100">一般會員</option> -->
 <!-- 				<option value="200">登山嚮導</option> -->
